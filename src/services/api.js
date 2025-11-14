e2109cc37940c9c8ffc2362e68e14b71def31c3a
mayzuk <githomazellijau@gmail.com>
2025-11-14 10:41:55 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const base = import.meta.env.VITE_API_URL || 'http://localhost:4000';
+const base = import.meta.env.VITE_API_URL || 'https://sistema-online-render-backend.onrender.com';
 
 export const api = axios.create({
   baseURL: base,
