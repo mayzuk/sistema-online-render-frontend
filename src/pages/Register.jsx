aeeb818d4b49e68ea12b7db4139b36c1e3dbd67d
mayzuk <githomazellijau@gmail.com>
2025-11-14 10:43:39 -0300

    Update Register.jsx

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -81,10 +81,7 @@ export default function Register() {
           className="w-full p-3 rounded border mb-4"
         />
 
-        <button 
-          type=submit 
-          className="w-full btn bg-gradient-to-r from-sky-300 to-indigo-300 font-semibold"
-        >
+        <button className="w-full btn bg-gradient-to-r from-sky-300 to-indigo-300 font-semibold">
           Criar conta
         </button>
       </form>
